ed7573b0a9533e3efce1ee18435a2b7c445d8b31
justansu <asherd@naver.com>
2025-03-24 14:47:50 +0900

    Add temperature sensor module (temperature.c/.h)

---
+++ STM32/Core/Src/temperature.c
@@ -0,0 +1,71 @@
+#include "temperature.h"
+#include <stdio.h>
+#include "lcd.h"
+static ADC_HandleTypeDef* local_hadc = NULL;
+
+volatile uint32_t adc_val = 0;
+volatile float latest_temperature = 0;
+volatile uint8_t temp_ready = 0;
+volatile uint8_t start_conversion_flag = 0;
+volatile uint8_t adc_ready_flag = 0;
+
+void temperatureInit(ADC_HandleTypeDef* hadc_ptr)
+{
+    local_hadc = hadc_ptr;
+}
+
+void temperatureTrigger(void)
+{
+    start_conversion_flag = 1;
+}
+
+void temperatureProcess(void)
+{
+    if (start_conversion_flag)
+    {
+        start_conversion_flag = 0;
+        HAL_ADC_Start_IT(local_hadc);
+    }
+
+    if (adc_ready_flag)
+    {
+        adc_ready_flag = 0;
+
+        adc_val = HAL_ADC_GetValue(local_hadc);
+        float vref = 3.3f;
+        float voltage = (float)adc_val * vref / 4095.0f;
+        latest_temperature = voltage * 100.0f;
+
+        temp_ready = 1;
+    }
+}
+
+__weak void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) 
+{
+    if (htim->Instance == TIM7)
+    {
+        temperatureTrigger();
+    }
+}
+
+__weak void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+    if (hadc->Instance == ADC1)
+    {
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+        adc_ready_flag = 1;
+    }
+}
+
+/*
+void Printing_t(int fan_count)
+{
+    char buffer[16];
+    lcdClearDisplay();
+    lcdSetCursor(0, 0);
+    lcdSendString("FAN ON");
+    lcdSetCursor(1, 0);
+    sprintf(buffer, "Temperture: %d", fan_count);
+    lcdSendString(buffer);
+}
+*/
