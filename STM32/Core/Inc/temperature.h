c9b3a1f8118ad7f89871b8f13c181e1b574f0d58
justansu <asherd@naver.com>
2025-03-24 14:47:50 +0900

    Add temperature sensor module (temperature.c/.h)

---
+++ STM32/Core/Inc/temperature.h
@@ -0,0 +1,14 @@
+#ifndef __TEMPERATURE_H__
+#define __TEMPERATURE_H__
+
+#include "main.h"
+
+extern volatile float latest_temperature;
+extern volatile uint8_t temp_ready;
+
+void temperatureInit(ADC_HandleTypeDef* hadc_ptr);
+void temperatureProcess(void);  // ADC 변환 완료 후 실행할 함수
+void temperatureTrigger(void);  // TIM 이벤트 발생 시 호출할 함수
+//void Printing_t(int fan_count);
+
+#endif /* __TEMPERATURE_H__ */
